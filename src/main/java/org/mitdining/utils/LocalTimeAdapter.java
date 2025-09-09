ddbb5e16d78c2836736646dde9c8e533c196ea2f
Roman Fedianovych <romaf@mit.edu>
2025-09-09 19:57:47 -0400

    Created TimeAdapter class for JSON serialization of LocalTime

--- src/main/java/org/mitdining/utils/LocalTimeAdapter.java
+++ src/main/java/org/mitdining/utils/LocalTimeAdapter.java
@@ -1,4 +1,20 @@
 package org.mitdining.utils;
 
-public class TimeAdapter {
+import com.google.gson.*;
+import java.lang.reflect.Type;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalTimeAdapter implements JsonSerializer<LocalTime>, JsonDeserializer<LocalTime> {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    @Override
+    public JsonElement serialize(LocalTime src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(src.format(FORMATTER));
+    }
+
+    @Override
+    public LocalTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        return LocalTime.parse(json.getAsString(), FORMATTER);
+    }
 }
