41f54fdb9e98adc97a91cc5293a80978686b8332
Roman Fedianovych <romaf@mit.edu>
2025-09-09 20:06:38 -0400

    Created a model class to represent open time ranges for dining halls

--- src/main/java/org/mitdining/model/TimeRange.java
+++ src/main/java/org/mitdining/model/TimeRange.java
@@ -1,4 +1,31 @@
 package org.mitdining.model;
 
+import java.time.LocalTime;
+import java.util.Date;
+
 public class TimeRange {
+    private final LocalTime open;
+    private final LocalTime close;
+
+    public TimeRange(LocalTime open, LocalTime close) {
+        this.open = open;
+        this.close = close;
+    }
+
+    boolean isOpen(LocalTime time) {
+        return time.isAfter(open) && time.isBefore(close);
+    }
+
+    public LocalTime getOpen() {
+        return open;
+    }
+
+    public LocalTime getClose() {
+        return close;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeRange{" + "open=" + open + ", close=" + close + '}';
+    }
 }
