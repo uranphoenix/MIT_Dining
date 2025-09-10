93bcc24f830ee594cd22ba823c3389867cddc19f
Roman Fedianovych <romaf@mit.edu>
2025-09-09 20:04:16 -0400

    Moved data loading logic to separate static method

--- src/main/java/org/mitdining/Main.java
+++ src/main/java/org/mitdining/Main.java
@@ -1,6 +1,9 @@
 package org.mitdining;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.mitdining.model.DiningHall;
+import org.mitdining.utils.LocalTimeAdapter;
 import org.mitdining.utils.Scrapper;
 import org.mitdining.utils.Storage;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -10,27 +13,25 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        /*
-        Scrapper.loadTimes("");
-        try {
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new Bot());
-        } catch (TelegramApiException e) {
-            System.out.println("Unable to create a bot");
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new Bot());
+    }
+
+    public static void loadDataToDatabase() throws IOException {
+        Storage storage = new Storage();
+        storage.initialize();
+
+        List<DiningHall> diningHalls = new ArrayList<>();
+        String [] hallNames = storage.getDiningHalls().stream().map(DiningHall::getName).toArray(String[]::new);
+        for (String hallName : hallNames) {
+            diningHalls.add(Scrapper.loadDiningHallTimes(hallName));
         }
-        */
-        //Storage storage = new Storage();
-        //storage.initialize();
-        try {
-            DiningHall nv = Scrapper.loadDiningHallTimes("new-vassar");
-            System.out.println(nv);
-            Storage.saveDiningHalls(List.of(nv));
-        } catch (IOException e) {
-            System.out.println(e.toString());
-        }
+        Storage.saveDiningHalls(diningHalls);
     }
 }
