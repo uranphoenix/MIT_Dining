921711bf5faf2ecf551c4a8ddb0b96390ca51476
Roman Fedianovych <romaf@mit.edu>
2025-09-09 20:00:02 -0400

    Added methods to retrieve open dining halls with corresponding time ranges

--- src/main/java/org/mitdining/utils/Storage.java
+++ src/main/java/org/mitdining/utils/Storage.java
@@ -2,6 +2,7 @@ package org.mitdining.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
 import com.google.gson.reflect.TypeToken;
 import org.mitdining.model.Day;
 import org.mitdining.model.DiningHall;
@@ -16,43 +17,54 @@ import java.util.stream.Collectors;
 
 public class Storage {
     private static final String DATA_FILE_PATH = System.getenv("DATA_FILE_PATH");
-//    private static final List<DiningHall> diningHalls = new ArrayList<>();
+    private List<DiningHall> diningHalls;
     private static final Gson gson = new GsonBuilder()
         .registerTypeAdapter(LocalTime.class, new LocalTimeAdapter())
         .setPrettyPrinting()
         .create();
 
+    public Storage() {
+
+    }
+
     public void initialize() {
         Path filePath = Path.of(DATA_FILE_PATH);
-//        TimeRange timeRange = new TimeRange(8.0f, 10.0f);
-//        TimeRange timeRange2 = new TimeRange(7.0f, 10.0f);
-        Map<Day, List<TimeRange>> schedule = new HashMap<>();
- //       schedule.put(Day.TUE, List.of(timeRange));
-        Map<Day, List<TimeRange>> schedule2 = new HashMap<>();
- //       schedule2.put(Day.TUE, List.of(timeRange2));
- //       diningHalls.add(new DiningHall(schedule));
-  //      diningHalls.add(new DiningHall(schedule2));
-
         try {
             if (!Files.exists(filePath)) {
                 throw new IOException("File not found: " + filePath);
             }
             TypeToken<List<DiningHall>> collectionType = new TypeToken<>(){};
-            List<DiningHall> din = gson.fromJson(Files.readString(filePath), collectionType);
-            for (DiningHall d : din) {
-                System.out.println(d);
-            }
+            diningHalls = gson.fromJson(Files.readString(filePath), collectionType);
         } catch (IOException e) {
             System.out.println(e.toString());
-            //System.err.println(e.getMessage());
         }
-
-
     }
 
 
-    public static List<DiningHall> getDiningHalls() {
-        return null;
+    public List<DiningHall> getDiningHalls() {
+        return diningHalls;
+    }
+
+    public Map<DiningHall, TimeRange> getOpenDiningHalls() {
+        Map<DiningHall, TimeRange> openDiningHallsAndTimes = new HashMap<>();
+        for (DiningHall diningHall : diningHalls) {
+            TimeRange open = diningHall.openTimeRange();
+            if (open != null) {
+                openDiningHallsAndTimes.put(diningHall, open);
+            }
+        }
+        return openDiningHallsAndTimes;
+    }
+
+    public Map<DiningHall, TimeRange> getOpenDiningHalls(Day day, LocalTime time) {
+        Map<DiningHall, TimeRange> openDiningHallsAndTimes = new HashMap<>();
+        for (DiningHall diningHall : diningHalls) {
+            TimeRange open = diningHall.openTimeRange(day, time);
+            if (open != null) {
+                openDiningHallsAndTimes.put(diningHall, open);
+            }
+        }
+        return openDiningHallsAndTimes;
     }
 
     public static void saveDiningHalls(List<DiningHall> diningHalls) {
